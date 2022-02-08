d43207defbc6d3e1ae342bdc7bba058c905495e4
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2022-02-08 08:55:09 -0800

    added parsing of which operations will be performed on each model

--- rainforest.py
+++ rainforest.py
@@ -19,43 +19,59 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', action='store_true',
-                        help='Verbose mode. Display loss graphs, precision graphs, confusion matrices etc')
+                        help='Verbose mode. Display loss graphs, precision'
+                        ' graphs, confusion matrices etc')
     parser.add_argument('-t', action='append', nargs='+', type=str,
-                        help='Training mode. Train the next N models that follow this flag.')
+                        help='Training mode. Train the next N models that'
+                        ' follow this flag.')
     parser.add_argument('-l', action='append', nargs='+', type=str,
-                        help='Load mode. Load the next N models that follow this flag from the working directory.')
+                        help='Load mode. Load the next N models that follow'
+                        ' this flag from the working directory.')
     parser.add_argument('-e', action='append', nargs='+', type=str,
-                        help='Evaluate model. Evaluate the next N models that follow this flag.')
+                        help='Evaluate model. Evaluate the next N models that'
+                        ' follow this flag.')
     parser.add_argument('-ts', action='append', nargs='+', type=str,
-                        help='Train and save mode. Train the next N models that follow this flag and save each.')
+                        help='Train and save mode. Train the next N models that'
+                        ' follow this flag and save each.')
 
 
     args = parser.parse_args()
     argc = len(sys.argv)
-    model_dict = dict()
     arg_dict = vars(args)
 
+    # we might want to switch to constructing separate lists for each argument 
+    # accepting lists of models for clarity.
+    model_dict = dict()
+
+    # parse command line arguments
     if argc > 1:
         # create dictionary storing model functionality when the script is run
         for arg in arg_dict:
             # ignore arguments that do not switch on functionality for model
+            # add them to this list
             if arg in ['v']:
                 continue
-            # for each list argument, set the corresponding boolean to True
-            for arch in arg_dict[arg].pop():
-                if arch not in model_dict:
-                    model_dict[arch] = dict()
-                model_dict[arch][arg] = True
+            # for each list argument, if the model was listed, set the 
+            # corresponding value to True
+            if arg_dict[arg] != None:
+                for arch in arg_dict[arg].pop():
+                    if arch not in model_dict:
+                        model_dict[arch] = dict()
+                    model_dict[arch][arg] = True
 
+    print(f'{model_dict=}')
+
+    # build dataframe containing training data
     train_dataframe = pd.read_csv('data/train_v2.csv').astype(str)
     train_dataframe['image_name'] += '.jpg'
     su.set_NLABELS(train_dataframe)
 
+    # since our task is multilabel classification, we must construct a multi 
+    # label binarizer to binarize each of the different labels
     mlb = MultiLabelBinarizer()
     mlb.fit(train_dataframe["tags"].str.split(" "))
-
-    print(f'{mlb.classes_=}')
     classes = mlb.classes_
+    print(f'{classes=}')
 
     ids = pd.DataFrame(mlb.fit_transform(train_dataframe['tags'].str.split(' ')), columns = classes)
 
@@ -66,8 +82,20 @@ def main():
 
     print(f'train_df.head =\n{train_df.head(n = 5)}')
 
-    for model_struct in model_dict:
-        model, is_training, is_evaluated, is_saved = model_struct
+    # compute operations on models specified at the command line
+    for model_name in model_dict:
+
+        # parse model_dict for the current model
+        for operation in model_dict[model_name]:
+            if operation == 't':
+                is_training = True
+            elif operation == 'l':
+                is_loaded = True
+            elif operation == 'e':
+                is_evaluated = True
+            elif operation == 'ts':
+                is_training = True
+                is_saved = True
 
         if is_training:
 
